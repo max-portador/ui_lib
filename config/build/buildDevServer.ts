8c8f5d6651fb78bc6ce60a820a27193858c67a3e
max-portador <46213974+max-portador@users.noreply.github.com>
2022-09-14 21:25:37 +0300

    add config

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,10 @@
+import {BuildOptions} from "./types/config";
+import type {Configuration as DevServerConfiguration} from "webpack-dev-server";
+
+export function buildDevServer(options: BuildOptions): DevServerConfiguration {
+    return {
+        port: options.port,
+        open: true, // автоматически открывать страницу в браузере после сборки
+        historyApiFallback: true, // позволяет проксировать запросы через index.html
+    }
+}
