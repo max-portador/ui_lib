1548f3467c3747b64337d19ebca400241af248d6
max-portador <46213974+max-portador@users.noreply.github.com>
2022-09-19 17:00:00 +0300

    add Navbar and AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import {Suspense} from "react";
-import {Link, Route, Routes} from "react-router-dom";
 import {Theme} from "app/providers/ThemeProvider/lib/ThemeContext";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 import './styles/index.scss'
 
 
@@ -13,23 +11,11 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
             <button onClick={toggleTheme}>
                 {`${theme === Theme.DARK ? 'Светлая' : 'Тёмная'} тема`}
             </button>
-
-            <div/>
-
-            <Link to={'/'}>Главная страница</Link>
-
-            <span> </span>
-
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
         </div>
     );
 };
---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,35 @@
+import {FC} from "react";
+import {Link, LinkProps} from "react-router-dom";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './AppLink.module.scss'
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+    RED = 'red'
+}
+
+interface AppLinkProps extends LinkProps{
+    className?: string;
+    theme?: AppLinkTheme;
+}
+
+const AppLink: FC<AppLinkProps> = (props) => {
+    const {
+        to,
+        className,
+        children,
+        theme = AppLinkTheme.PRIMARY,
+        ...otherProps} = props
+    return (
+        <Link
+            to={to}
+            className={classNames(cls.appLink, {}, [className, cls[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+};
+
+export { AppLink }
