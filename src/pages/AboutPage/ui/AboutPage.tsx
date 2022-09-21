221c32b3cb6c348319491aefdaba7d44f9f6ba9d
portador <deus7@protonmail.com>
2022-09-21 19:58:56 +0300

    add i18n

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,7 +1,11 @@
+import {useTranslation} from "react-i18next";
+
 const AboutPage = () => {
+    const {t} = useTranslation()
+
     return (
         <div>
-            ABOUT PAGE
+            {t('О сайте')}
         </div>
     );
 };
---
+++ src/shared/ui/LangSwitcher/ui/LangSwitcher.tsx
@@ -0,0 +1,29 @@
+import {useTranslation} from "react-i18next";
+import cls from './LangSwitcher.module.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {Button, ThemeButton} from "shared/ui/Button/Button";
+
+interface LangSwitcherProps {
+    className?: string;
+}
+
+const LangSwitcher = ({className}: LangSwitcherProps) => {
+    const {t, i18n} = useTranslation();
+
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+    }
+
+    return (
+        <Button
+            className={classNames(cls.langSwitcher, {}, [className])}
+            theme={ThemeButton.CLEAR}
+            onClick={toggle}
+        >
+
+            {t('Язык')}
+        </Button>
+    )
+};
+
+export {LangSwitcher}
