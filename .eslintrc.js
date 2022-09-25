a641589573eee8b92245855f4dc1ab000becd6d9
portador <deus7@protonmail.com>
2022-09-25 16:00:02 +0300

    add stylelint and i18n for EsLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     extends: [
         'plugin:react/recommended',
         'airbnb',
+        'plugin:i18next/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -19,6 +20,7 @@ module.exports = {
     plugins: [
         'react',
         '@typescript-eslint',
+        'i18next',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -49,6 +51,7 @@ module.exports = {
             'always',
         ],
         'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
     globals: {
         __IS_DEV__: true,
--- src/shared/ui/LangSwitcher/ui/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,29 +1,28 @@
-import {useTranslation} from "react-i18next";
-import cls from './LangSwitcher.module.scss';
-import {classNames} from "shared/lib/classNames/classNames";
-import {Button, ThemeButton} from "shared/ui/Button/Button";
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 interface LangSwitcherProps {
     className?: string;
 }
 
-const LangSwitcher = ({className}: LangSwitcherProps) => {
-    const {t, i18n} = useTranslation();
+const LangSwitcher = ({ className }: LangSwitcherProps) => {
+    const { t, i18n } = useTranslation();
 
     const toggle = () => {
-        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
-    }
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    };
 
     return (
         <Button
-            className={classNames(cls.langSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             theme={ThemeButton.CLEAR}
             onClick={toggle}
         >
 
             {t('Язык')}
         </Button>
-    )
+    );
 };
 
-export {LangSwitcher}
+export { LangSwitcher };
