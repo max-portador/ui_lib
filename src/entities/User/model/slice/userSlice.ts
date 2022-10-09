ad6faa63849c600ae67ddafa70b39be90f749220
portador <deus7@protonmail.com>
2022-10-09 22:21:16 +0300

    add json-server, user entity

---
+++ src/entities/User/model/slice/userSlice.ts
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { UserSchema } from '../types/user';
+
+const initialState: UserSchema = {
+};
+
+export const counterSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+    },
+});
+
+export const { actions: userActions } = counterSlice;
+export const { reducer: userReducer } = counterSlice;
